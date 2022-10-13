eca570224a73866cb4b0b0715d9bab329a8f298c
Abi Castro <abixcastro@gmail.com>
2022-10-13 12:45:33 -0700

    Update mintToken.js

--- mintToken.js
+++ mintToken.js
@@ -1,18 +1,7 @@
 const { TokenMintTransaction } = require('@hashgraph/sdk');
 
 const mintToken = async (client, tokenId, amount, supplyKey) => {
-  const mintTokenTxn = new TokenMintTransaction()
-    .setTokenId(tokenId)
-    .setAmount(amount)
-    .freezeWith(client);
-
-  const mintTokenTxnSigned = await mintTokenTxn.sign(supplyKey);
-
-  // submit txn to hedera network
-  const txnResponse = await mintTokenTxnSigned.execute(client);
-
-  const mintTokenRx = await txnResponse.getReceipt(client);
-  const mintTokenStatus = mintTokenRx.status.toString();
+// todo: add this
 
   console.log(`Token mint was a ${mintTokenStatus}`);
 };
